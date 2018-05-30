87210ba4fb4acd5d6410777bf5c58a318c9b0ba6
Jean-Michel Drolet <jmdrolet@coveo.com>
2018-05-30 13:11:41 -0400

    Add DependentHierarchical component

--- src/Components.ts
+++ src/Components.ts
@@ -1,2 +1,3 @@
 export { ShowMore } from './components/ShowMore/ShowMore';
 export { RangePicker } from './components/RangePicker/RangePicker';
+export { DependentHierarchicalFacet } from './components/DependentHierarchicalFacet/DependentHierarchicalFacet';
