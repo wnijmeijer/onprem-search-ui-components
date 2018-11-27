f6858e3d0915729a80616e653349c6a080838be5
Yassine Lakhdar <ylakhdar@coveo.com>
2018-11-27 11:33:34 -0500

    Add SearchHistory component

--- src/Components.ts
+++ src/Components.ts
@@ -2,3 +2,4 @@ export { ShowMore } from './components/ShowMore/ShowMore';
 export { RangePicker } from './components/RangePicker/RangePicker';
 export { DependentHierarchicalFacet } from './components/DependentHierarchicalFacet/DependentHierarchicalFacet';
 export { SalesforceCommunityResultLink } from './components/SalesforceCommunityResultLink/SalesforceCommunityResultLink';
+export { SearchHistory } from './components/SearchHistory/SearchHistory';
