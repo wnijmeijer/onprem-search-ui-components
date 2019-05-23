901894e1aee84b4bec8d51e97aa5de684d5b5b27
Jean-Michel Drolet <jmdrolet@coveo.com>
2019-05-22 23:15:11 -0400

    feature: add SortDropdown

--- src/Components.ts
+++ src/Components.ts
@@ -1,5 +1,6 @@
 export { ShowMore } from './components/ShowMore/ShowMore';
 export { RangePicker } from './components/RangePicker/RangePicker';
+export { SortDropdown } from './components/SortDropdown/SortDropdown';
 export { DependentHierarchicalFacet } from './components/DependentHierarchicalFacet/DependentHierarchicalFacet';
 export { SalesforceCommunityResultLink } from './components/SalesforceCommunityResultLink/SalesforceCommunityResultLink';
 export { SearchHistory } from './components/SearchHistory/SearchHistory';
